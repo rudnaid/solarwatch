4e6167ef4e554092fe52e04ef2795494903c47bf
rudnaid <david.rudnai@gmail.com>
2025-01-29 14:58:25 +0100

    add Controller Advice class for exception handling

--- src/main/java/com/codecool/solarwatch/exception/SolarWatchControllerAdvice.java
+++ src/main/java/com/codecool/solarwatch/exception/SolarWatchControllerAdvice.java
@@ -1,4 +1,40 @@
 package com.codecool.solarwatch.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
 public class SolarWatchControllerAdvice {
+
+    @ExceptionHandler(InvalidCityException.class)
+    public ResponseEntity<String> handleInvalidCityException(InvalidCityException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(InvalidDateException.class)
+    public ResponseEntity<String> handleInvalidDateException(InvalidDateException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(InvalidTimezoneException.class)
+    public ResponseEntity<String> handleInvalidTimezoneException(InvalidTimezoneException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(InvalidRequestException.class)
+    public ResponseEntity<String> handleInvalidRequestException(InvalidRequestException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(UnknownApiErrorException.class)
+    public ResponseEntity<String> handleUnknownApiErrorException(UnknownApiErrorException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_GATEWAY);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGenericException(Exception ex) {
+        return new ResponseEntity<>("An unexpected error occurred.", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
