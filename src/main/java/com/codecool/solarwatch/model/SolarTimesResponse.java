2bc700358f639f733f1bdaa70ea466a2dff3afa5
rudnaid <david.rudnai@gmail.com>
2025-01-30 08:09:02 +0100

    refactor: remove unnecessary Jsonproperty annotation

--- src/main/java/com/codecool/solarwatch/model/SolarTimesResponse.java
+++ src/main/java/com/codecool/solarwatch/model/SolarTimesResponse.java
@@ -1,9 +1,6 @@
 package com.codecool.solarwatch.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record SolarTimesResponse(
-        @JsonProperty("results") SolarTimes results
-) { }
+public record SolarTimesResponse(SolarTimes results) { }
