21ef814961d201369ab1b6a49a0d413a9e61e14b
rudnaid <david.rudnai@gmail.com>
2025-02-12 11:11:52 +0100

    add City entity

--- src/main/java/com/codecool/solarwatch/model/City.java
+++ src/main/java/com/codecool/solarwatch/model/City.java
@@ -1,4 +1,40 @@
 package com.codecool.solarwatch.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class City {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    
+    private String id;
+    private String name;
+    private String country;
+    private String state;
+    private double lat;
+    private double lon;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public double getLat() {
+        return lat;
+    }
+
+    public double getLon() {
+        return lon;
+    }
 }
