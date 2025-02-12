6f01857aa912671613948e7e6f620c798a7e6ea3
rudnaid <david.rudnai@gmail.com>
2025-02-12 12:59:23 +0100

    add City and SunsetSunrise repositories

--- src/main/java/com/codecool/solarwatch/repository/CityRepository.java
+++ src/main/java/com/codecool/solarwatch/repository/CityRepository.java
@@ -1,4 +1,14 @@
 package com.codecool.solarwatch.repository;
 
-public interface CityRepository {
+import com.codecool.solarwatch.model.City;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CityRepository extends JpaRepository<City, String> {
+
+    Optional<City> findByName(String name);
+
 }
--- src/main/java/com/codecool/solarwatch/repository/SunriseSunsetRepository.java
+++ src/main/java/com/codecool/solarwatch/repository/SunriseSunsetRepository.java
@@ -1,4 +1,14 @@
 package com.codecool.solarwatch.repository;
 
-public interface SunriseSunsetRepository {
+import com.codecool.solarwatch.model.City;
+import com.codecool.solarwatch.model.SunriseSunset;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface SunriseSunsetRepository extends JpaRepository<SunriseSunset, String> {
+
+    Optional<SunriseSunset> findSunriseSunsetByCityId(String cityId);
 }
