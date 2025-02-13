39691547be5ca1f2c999ddfbad58948730ab74cf
rudnaid <david.rudnai@gmail.com>
2025-02-13 11:42:45 +0100

    refactor: use lombok for getters and setters

--- src/main/java/com/codecool/solarwatch/model/SunriseSunset.java
+++ src/main/java/com/codecool/solarwatch/model/SunriseSunset.java
@@ -1,31 +1,34 @@
 package com.codecool.solarwatch.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
 
+import java.time.LocalDate;
 import java.time.ZonedDateTime;
 
 @Entity
+@Getter
+@Setter
 public class SunriseSunset {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-
     private String id;
+
+    @CreationTimestamp
+    @Column(columnDefinition = "TIMESTAMP WITH TIME ZONE")
+    private LocalDate createdAt;
+
+    @Column(columnDefinition = "TIMESTAMP WITH TIME ZONE")
     private ZonedDateTime sunrise;
+
+    @Column(columnDefinition = "TIMESTAMP WITH TIME ZONE")
+
     private ZonedDateTime sunset;
 
     @ManyToOne
     private City city;
 
-    public ZonedDateTime getSunrise() {
-        return sunrise;
-    }
-
-    public ZonedDateTime getSunset() {
-        return sunset;
-    }
-
-    public City getCity() {
-        return city;
-    }
 }
