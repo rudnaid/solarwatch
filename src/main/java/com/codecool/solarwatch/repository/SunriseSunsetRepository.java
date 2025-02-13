5e9ed41c3c333d5d1fd0356ebae40929e684ba22
rudnaid <david.rudnai@gmail.com>
2025-02-13 11:46:17 +0100

    refactor: formatting

--- src/main/java/com/codecool/solarwatch/repository/SunriseSunsetRepository.java
+++ src/main/java/com/codecool/solarwatch/repository/SunriseSunsetRepository.java
@@ -5,10 +5,11 @@ import com.codecool.solarwatch.model.SunriseSunset;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.Optional;
 
 @Repository
 public interface SunriseSunsetRepository extends JpaRepository<SunriseSunset, String> {
 
-    Optional<SunriseSunset> findSunriseSunsetByCityId(String cityId);
+    Optional<SunriseSunset> findByCityAndCreatedAt(City city, LocalDate date);
 }
