7b0b6adf7eef23e92f4140a1f0d1e0b681832018
rudnaid <david.rudnai@gmail.com>
2025-02-13 11:41:29 +0100

    add lombok dependency

--- src/main/java/com/codecool/solarwatch/model/SolarTimes.java
+++
@@ -1,9 +0,0 @@
-package com.codecool.solarwatch.model;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-import java.time.ZonedDateTime;
-
-@JsonIgnoreProperties(ignoreUnknown = true)
-public record SolarTimes(ZonedDateTime sunrise, ZonedDateTime sunset) {
-}
