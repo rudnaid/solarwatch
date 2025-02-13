18e5c8e799715312c9715c5d84340b2bcf9b32a6
rudnaid <david.rudnai@gmail.com>
2025-02-13 11:44:48 +0100

    refactor: remove unnecessary fields

--- src/main/java/com/codecool/solarwatch/model/SunriseSunsetDTO.java
+++ src/main/java/com/codecool/solarwatch/model/SunriseSunsetDTO.java
@@ -5,14 +5,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.time.ZonedDateTime;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record SolarTimes(
+public record SunriseSunsetDTO(
         ZonedDateTime sunrise,
-        ZonedDateTime sunset,
-        ZonedDateTime solarNoon,
-        ZonedDateTime civilTwilightBegin,
-        ZonedDateTime civilTwilightEnd,
-        ZonedDateTime nauticalTwilightBegin,
-        ZonedDateTime nauticalTwilightEnd,
-        ZonedDateTime astronomicalTwilightBegin,
-        ZonedDateTime astronomicalTwilightEnd
+        ZonedDateTime sunset
 ) { }
