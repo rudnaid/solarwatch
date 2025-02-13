4ea6ccbc4e8eb75a651beb8a3cd5fb158f7fb953
rudnaid <david.rudnai@gmail.com>
2025-02-13 11:47:10 +0100

    refactor: classname changed to SunriseSunsetResponseDTO

--- src/main/java/com/codecool/solarwatch/model/SunriseSunsetResponseDTO.java
+++ src/main/java/com/codecool/solarwatch/model/SunriseSunsetResponseDTO.java
@@ -3,4 +3,4 @@ package com.codecool.solarwatch.model;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record SolarTimesResponse(SolarTimes results) { }
+public record SunriseSunsetResponseDTO(SunriseSunsetDTO results) { }
