0b5ca34f6978a1f26640e2ec3996a8c406ca6e0b
rudnaid <david.rudnai@gmail.com>
2025-02-15 09:58:07 +0100

    refactor: remove unnecessary createdAt column

--- src/main/java/com/codecool/solarwatch/model/SunriseSunset.java
+++ src/main/java/com/codecool/solarwatch/model/SunriseSunset.java
@@ -3,9 +3,7 @@ package com.codecool.solarwatch.model;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.annotations.CreationTimestamp;
 
-import java.time.LocalDate;
 import java.time.ZonedDateTime;
 
 @Entity
@@ -17,15 +15,10 @@ public class SunriseSunset {
     @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
 
-    @CreationTimestamp
-    @Column(columnDefinition = "TIMESTAMP WITH TIME ZONE")
-    private LocalDate createdAt;
-
     @Column(columnDefinition = "TIMESTAMP WITH TIME ZONE")
     private ZonedDateTime sunrise;
 
     @Column(columnDefinition = "TIMESTAMP WITH TIME ZONE")
-
     private ZonedDateTime sunset;
 
     @ManyToOne
