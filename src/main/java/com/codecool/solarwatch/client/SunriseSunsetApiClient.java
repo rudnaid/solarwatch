09b44ff4a2960b72fe4a8fab5fc3403e77edd111
rudnaid <david.rudnai@gmail.com>
2025-02-15 09:54:56 +0100

    refactor: change RestTemplate to WebClient add UriComponentBuilder to build url add javadoc

--- src/main/java/com/codecool/solarwatch/client/SunriseSunsetApiClient.java
+++ src/main/java/com/codecool/solarwatch/client/SunriseSunsetApiClient.java
@@ -1,26 +1,53 @@
 package com.codecool.solarwatch.client;
 
-import com.codecool.solarwatch.exception.UnknownApiErrorException;
 import com.codecool.solarwatch.model.SunriseSunsetResponseDTO;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @Component
 public class SunriseSunsetApiClient {
-    private final RestTemplate restTemplate;
+    private final WebClient webClient;
 
-    public SunriseSunsetApiClient(RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
+    public SunriseSunsetApiClient(WebClient webClient) {
+        this.webClient = webClient;
     }
 
+    /**
+     * Call external API to get the sunrise/sunset times according to geo-coordinates of a city.
+     *
+     * @param lat Latitude
+     * @param lng Longitude
+     * @param date The date the requested sunrise/sunset times.
+     * @param tzid Time Zone ID.
+     * @param formatted 0: ZonedDateTime format, 1: YYYY-MM-DD-hhss format.
+     * @return SunriseSunsetResponseDTO, results are extracted with SunriseSunsetDTO.
+     */
+
     public SunriseSunsetResponseDTO getSunriseSunsetByCoordinates(double lat, double lng, String date, String tzid, int formatted) {
-        String url = String.format("https://api.sunrise-sunset.org/json?lat=%f&lng=%f&date=%s&tzid=%s&formatted=%d", lat, lng, date, tzid, formatted);
+
+        String url = UriComponentsBuilder.fromUriString("https://api.sunrise-sunset.org/json")
+                .queryParam("lat", lat)
+                .queryParam("lng", lng)
+                .queryParam("date", date)
+                .queryParam("tzid", tzid)
+                .queryParam("formatted", formatted)
+                .build()
+                .toUriString();
 
         try {
-            return restTemplate.getForObject(url, SunriseSunsetResponseDTO.class);
+            return webClient.get()
+                    .uri(url)
+                    .accept(MediaType.APPLICATION_JSON)
+                    .retrieve()
+                    .bodyToMono(SunriseSunsetResponseDTO.class)
+                    .block();
+
         } catch (HttpClientErrorException ex) {
-            throw new UnknownApiErrorException();
+            throw new ResponseStatusException(ex.getStatusCode(), ex.getResponseBodyAsString());
         }
     }
 }
