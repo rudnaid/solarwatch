f55ad292740cd9453cc0f5d4fb9aabdf35373422
rudnaid <david.rudnai@gmail.com>
2025-02-27 12:23:43 +0100

    feat(user): create UserEntity

--- src/main/java/com/codecool/solarwatch/model/entity/UserEntity.java
+++ src/main/java/com/codecool/solarwatch/model/entity/UserEntity.java
@@ -1,6 +1,24 @@
 package com.codecool.solarwatch.model.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
 import java.util.Set;
 
-public record UserEntity(String username, String password, Set<Role> roles) {
+@Entity
+@Data
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+
+    @ElementCollection(targetClass = Role.class, fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
+    @Column(name = "role")
+    @Enumerated(EnumType.STRING)
+    private Set<Role> roles;
 }
