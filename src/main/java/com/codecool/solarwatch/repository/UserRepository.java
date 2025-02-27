8c66977107203a6e7333243f48699e171eab0b57
rudnaid <david.rudnai@gmail.com>
2025-02-27 12:25:22 +0100

    feat(user): create UserRepository

---
+++ src/main/java/com/codecool/solarwatch/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.codecool.solarwatch.repository;
+
+import com.codecool.solarwatch.model.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    Optional<UserEntity> findByUsername(String username);
+}
