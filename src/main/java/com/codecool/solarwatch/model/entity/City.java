7289e1f7e2526eddac94d0d8472161ecffc67f03
rudnaid <david.rudnai@gmail.com>
2025-03-01 10:49:14 +0100

    feat(dto): add CityDTO for city related CRUD operations

--- src/main/java/com/codecool/solarwatch/model/entity/City.java
+++ src/main/java/com/codecool/solarwatch/model/entity/City.java
@@ -1,14 +1,12 @@
 package com.codecool.solarwatch.model.entity;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
 import java.util.List;
 
 @Entity
-@Getter
-@Setter
+@Data
 public class City {
 
     @Id
