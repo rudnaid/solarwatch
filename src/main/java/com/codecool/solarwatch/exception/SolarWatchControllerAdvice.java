2b751f019d794a85f620ceaf671b2eba6a3fe795
rudnaid <david.rudnai@gmail.com>
2025-03-01 10:52:06 +0100

    refactor(exceptions): remove unnecessary imports

--- src/main/java/com/codecool/solarwatch/exception/SolarWatchControllerAdvice.java
+++ src/main/java/com/codecool/solarwatch/exception/SolarWatchControllerAdvice.java
@@ -2,11 +2,8 @@ package com.codecool.solarwatch.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.server.ResponseStatusException;
 
 @ControllerAdvice
