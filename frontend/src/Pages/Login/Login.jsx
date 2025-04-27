d218fc6ea8380f3d36aa6307e7daa56f591b3084
rudnaid <david.rudnai@gmail.com>
2025-04-27 13:36:25 +0200

    feat(login): add logic to handle cancellation of login

--- frontend/src/Pages/Login/Login.jsx
+++ frontend/src/Pages/Login/Login.jsx
@@ -1,10 +1,17 @@
 import React from "react";
-import LoginForm from "../../Components/LoginForm/LoginForm.jsx";
+import LoginForm from "../../Components/Forms/LoginForm.jsx";
+import {useNavigate} from "react-router-dom";
 
 const Login = () => {
+  const navigate = useNavigate();
+
+  const handleCancel = () => {
+    navigate("/");
+  }
+
   return (
-    <div className="control">
-      <LoginForm />
+    <div className="flex justify-center pt-12 min-h-screen">
+      <LoginForm onCancel={handleCancel} />
     </div>
   )
 }
