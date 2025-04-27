4a935160c3d5fbbef6c2acab3902c5c85c29d105
rudnaid <david.rudnai@gmail.com>
2025-04-27 14:18:35 +0200

    refactor(controller): return DTO-s instead of ResponseEntity

--- backend/src/main/java/com/codecool/solarwatch/controller/SolarWatchController.java
+++ backend/src/main/java/com/codecool/solarwatch/controller/SolarWatchController.java
@@ -1,21 +1,17 @@
 package com.codecool.solarwatch.controller;
 
 import com.codecool.solarwatch.model.dto.CityDTO;
+import com.codecool.solarwatch.model.dto.CityDetailsDTO;
 import com.codecool.solarwatch.model.dto.SunriseSunsetDTO;
 import com.codecool.solarwatch.service.SolarWatchService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
-
-import java.time.LocalDate;
-import java.time.format.DateTimeParseException;
 
 @RestController
 @RequestMapping("/api/solarwatch")
 public class SolarWatchController {
+
     private final SolarWatchService solarWatchService;
 
     @Autowired
@@ -30,77 +26,53 @@ public class SolarWatchController {
      * @param date      Date of requested sunrise/sunset data. When omitted, default is current date.
      * @param tzid      Time Zone ID.
      * @param formatted 0: ZonedDateTime format, 1: YYYY-MM-DD-hhss format.
-     * @return CityDTO object wrapped in a ResponseEntity.
+     * @return CityDetailsDTO object.
      */
-
     @GetMapping("/times")
     @PreAuthorize("hasRole('USER')")
-    public ResponseEntity<?> getSolarTimes(
+    public CityDetailsDTO getSolarTimes(
             @RequestParam String city,
             @RequestParam(required = false) String date,
             @RequestParam(defaultValue = "UTC") String tzid,
             @RequestParam(defaultValue = "0") int formatted) {
 
-        LocalDate requestDate;
-
-        if (date == null) {
-            requestDate = LocalDate.now();
-        } else {
-            try {
-                requestDate = LocalDate.parse(date);
-            } catch (DateTimeParseException e) {
-                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
-            }
-        }
-
-        var result = solarWatchService.getSolarTimes(city, requestDate, tzid, formatted);
-
-        return ResponseEntity.ok(result);
+        return solarWatchService.getSolarTimes(city, date, tzid, formatted);
     }
 
     @PostMapping("/cities")
     @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<?> addCity(@RequestBody CityDTO city) {
-        var result = solarWatchService.createCityFromGeoCodingResponse(city.getName());
-
-        return ResponseEntity.ok(result);
+    public void addCity(@RequestBody CityDTO city) {
+        solarWatchService.createCityFromGeoCodingResponse(city.getName());
     }
 
     @PutMapping("/cities")
     @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<?> updateCity(@RequestBody CityDTO city) {
-        var result = solarWatchService.updateCity(city);
-
-        return ResponseEntity.ok(result);
+    public CityDTO updateCity(@RequestBody CityDTO city) {
+        return solarWatchService.updateCity(city);
     }
 
     @DeleteMapping("/cities")
     @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<?> deleteCity(@RequestBody CityDTO city) {
+    public void deleteCity(@RequestBody CityDTO city) {
         solarWatchService.deleteCity(city);
-        return ResponseEntity.ok(HttpStatus.OK);
     }
 
     @PostMapping("/sunrisesunset")
     @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<?> createSunriseSunset(@RequestBody SunriseSunsetDTO sunriseSunset) {
-        var result = solarWatchService.createSunriseSunset(sunriseSunset);
-
-        return ResponseEntity.ok(result);
+    public SunriseSunsetDTO createSunriseSunset(@RequestBody SunriseSunsetDTO sunriseSunset) {
+        return solarWatchService.createSunriseSunset(sunriseSunset);
     }
 
 
     @PutMapping("/sunrisesunset/{id}")
     @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<?> updateSunriseSunset(@PathVariable Long id, @RequestBody SunriseSunsetDTO updatedSunriseSunsetDTO) {
-        var result = solarWatchService.updateSunriseSunset(id, updatedSunriseSunsetDTO);
-        return ResponseEntity.ok(result);
+    public SunriseSunsetDTO updateSunriseSunset(@PathVariable Long id, @RequestBody SunriseSunsetDTO updatedSunriseSunsetDTO) {
+        return solarWatchService.updateSunriseSunset(id, updatedSunriseSunsetDTO);
     }
 
     @DeleteMapping("/sunrisesunset/{id}")
     @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<?> deleteSunriseSunset(@PathVariable Long id) {
+    public void deleteSunriseSunset(@PathVariable Long id) {
         solarWatchService.deleteSunriseSunset(id);
-        return ResponseEntity.noContent().build();
     }
 }
