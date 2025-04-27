e695e4ce4f58655fe9e5af1465d0101f53beb626
rudnaid <david.rudnai@gmail.com>
2025-04-27 14:25:11 +0200

    feat(exception): create custom exception GeoCodingApiResponseException to handle external API exceptions

---
+++ backend/src/main/java/com/codecool/solarwatch/exception/GeoCodingApiResponseException.java
@@ -0,0 +1,7 @@
+package com.codecool.solarwatch.exception;
+
+public class GeoCodingApiResponseException extends RuntimeException {
+  public GeoCodingApiResponseException(String message) {
+    super(message);
+  }
+}
