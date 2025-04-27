c3567fe99462638a9b8260f1ce87501c2be23924
rudnaid <david.rudnai@gmail.com>
2025-04-27 13:35:26 +0200

    feat(loading): add new LoadingComponent and remove old Loading component

--- frontend/src/Components/Loading/Loading.jsx
+++
@@ -1,7 +0,0 @@
-export const Loading = () => {
-  return (
-    <div className="loading">
-      Loading...
-    </div>
-  )
-};
---
+++ frontend/src/Components/Loading/LoadingComponent.jsx
@@ -0,0 +1,9 @@
+const LoadingComponent = () => {
+  return (
+    <div >
+      <span className="loading loading-dots loading-l"></span>
+    </div>
+  )
+};
+
+export default LoadingComponent;
