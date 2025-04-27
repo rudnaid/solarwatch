9d6d0e47a264d99b95c9cf48d43b88eb3a20b529
rudnaid <david.rudnai@gmail.com>
2025-04-27 13:34:36 +0200

    feat(layout): adjust styling for navbar

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,19 +1,36 @@
-import {Link} from "react-router-dom";
 import React from "react";
+import {Link} from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="navbar">
 
-      <Link className="btn btn-ghost txt-xl" to="/register">
-        Register
-      </Link>
+    <div className="navbar bg-base-100 shadow-sm">
 
-      <Link className="btn btn-ghost txt-xl" to="/login">
-        Login
-      </Link>
+      <div className="flex-1">
+        <Link className="btn btn-ghost text-xl" to={"/"}>
+          SolarWatch
+        </Link>
+      </div>
 
-    </nav>
+      <div className="flex-none">
+        <ul className="menu menu-horizontal px-1">
+
+          <li>
+            <Link className="btn btn-ghost txt-xl" to="/register">
+              Register
+            </Link>
+          </li>
+
+          <li>
+            <Link className="btn btn-ghost txt-xl" to="/login">
+              Login
+            </Link>
+          </li>
+
+        </ul>
+      </div>
+    </div>
+
   )
 }
 
