e0b0f50663c50043df3a1709e2fd6c21b406ab99
rudnaid <david.rudnai@gmail.com>
2025-04-27 13:34:02 +0200

    feat(layout): add welcome content and adjust styling

--- frontend/src/Pages/Layout/Layout.jsx
+++ frontend/src/Pages/Layout/Layout.jsx
@@ -1,14 +1,36 @@
-import React, { useEffect, useRef, useState } from 'react';
-import {Outlet} from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Navbar from "../../Components/Navbar/Navbar.jsx";
+import Footer from "../../Components/Footer/Footer.jsx";
 
 const Layout = () => {
+  const location = useLocation();
+
+  const renderWelcomeContent = () => {
+    if (location.pathname === '/') {
+      return (
+        <div className="flex flex-col pt-12 items-center">
+          <h1>Welcome to SolarWatch!</h1>
+          <p>Log in or register to search for sunrise and sunset times of a city of your choice!</p>
+        </div>
+      );
+    }
+    return null;
+  };
+
   return (
-    <div>
+    <div className="flex flex-col min-h-screen">
+
       <Navbar />
-      <Outlet />
+
+      <main className="flex-grow">
+        {renderWelcomeContent()}
+        <Outlet />
+      </main>
+
+      <Footer />
+
     </div>
   );
-}
+};
 
 export default Layout;
