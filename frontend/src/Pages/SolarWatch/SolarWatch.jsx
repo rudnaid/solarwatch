7680a599d26655e3f0dca2ad68d53e2b61f3c22c
rudnaid <david.rudnai@gmail.com>
2025-04-27 13:40:27 +0200

    feat(solarwatch): combine recieved data into one state and add loading handling

--- frontend/src/Pages/SolarWatch/SolarWatch.jsx
+++ frontend/src/Pages/SolarWatch/SolarWatch.jsx
@@ -1,28 +1,50 @@
-import { useState} from "react";
+import {useState} from "react";
 import {getSolarTimes} from "../../Service/apiService.js";
-import {SearchBar} from "../../Components/SearchBar/SearchBar.jsx";
 import {useAuth} from "../../Context/AuthContext.jsx";
+import SolarWatchContent from "../../Components/Content/SolarWatchContent.jsx";
+import LoadingComponent from "../../Components/Loading/LoadingComponent.jsx";
+import SearchBar from "../../Components/SearchBar/SearchBar.jsx";
 
 export const SolarWatch = () => {
   const {user} = useAuth();
-  const [city, setCity] = useState('');
-  const [country, setCountry] = useState('');
-  const [sunrise, setSunrise] = useState('');
-  const [sunset, setSunset] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [searchCompleted, setSearchCompleted] = useState(false);
+  const [solarData, setSolarData] = useState({
+    city: 'city',
+    country: 'country',
+    sunrise: 'sunrise',
+    sunset: 'sunset'
+  });
 
   const handleSearch = async (city, date) => {
     try {
+      setLoading(true);
+      setSearchCompleted(false);
+
       const result = await getSolarTimes(user, city, date);
 
-      setCity(result.name);
-      setCountry(result.country)
-      setSunrise(result.sunriseSunset.sunrise);
-      setSunset(result.sunriseSunset.sunset);
+      setSolarData({
+        city: result.name,
+        country: result.country,
+        sunrise: result.sunriseSunset.sunrise,
+        sunset: result.sunriseSunset.sunset,
+        ...result
+      });
 
+      setSearchCompleted(true);
+      setLoading(false);
     } catch (error) {
       console.error(error.message);
+      setLoading(false);
+      setSearchCompleted(false);
     }
-  }
+  };
+
+  if (loading) return (
+    <div className="flex items-center justify-center h-screen">
+      <LoadingComponent />
+    </div>
+  );
 
   return (
     <div>
