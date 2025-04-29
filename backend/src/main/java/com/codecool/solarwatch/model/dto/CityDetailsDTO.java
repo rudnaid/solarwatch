e55b2825e8c4188f71f2c04d170f6b3bcd9002bf
rudnaid <david.rudnai@gmail.com>
2025-04-29 09:37:21 +0200

    refactor(dto): change CityDetailsDTO to be a record class

--- backend/src/main/java/com/codecool/solarwatch/model/dto/CityDetailsDTO.java
+++ backend/src/main/java/com/codecool/solarwatch/model/dto/CityDetailsDTO.java
@@ -1,12 +1,8 @@
 package com.codecool.solarwatch.model.dto;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
-public class CityDetailsDTO {
-    private String name;
-    private String country;
-    private SunriseSunsetDTO sunriseSunset;
+public record CityDetailsDTO(
+        String name,
+        String country,
+        SunriseSunsetDTO sunriseSunset
+) {
 }
