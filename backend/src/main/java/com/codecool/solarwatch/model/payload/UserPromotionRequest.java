327b28238512160bc1d48847db5ea9bd800d3395
rudnaid <david.rudnai@gmail.com>
2025-04-29 09:36:44 +0200

    refactor(dto): change UserPromotionRequest to be a record class

--- backend/src/main/java/com/codecool/solarwatch/model/payload/UserPromotionRequest.java
+++ backend/src/main/java/com/codecool/solarwatch/model/payload/UserPromotionRequest.java
@@ -1,8 +1,6 @@
 package com.codecool.solarwatch.model.payload;
 
-import lombok.Data;
-
-@Data
-public class UserPromotionRequest {
-    private String username;
+public record UserPromotionRequest(
+        String username
+) {
 }
